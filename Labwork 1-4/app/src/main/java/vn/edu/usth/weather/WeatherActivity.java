dab13cee634dff8dadd0daa3fb3df08a06fe5f3c
dinhphuc1706 <dinhphucs.16@gmail.com>
2021-10-13 01:08:22 +0700

    Update Labwork 1-4

---
+++ Labwork 1-4/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,56 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment ff = ForecastFragment.newInstance("", "");
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, ff).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weather","OnStart() Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weather","OnStop() Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather","OnDestroy() Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Weather","OnPause() Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weather","OnResume() Called");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Log.i("Weather","OnCreate() Called");
+    }
+}
